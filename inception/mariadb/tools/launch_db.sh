2397a81b9fa55425ba69d52b0d2eee2a327cedb7
Ayoub Bouras <aybouras@e3r9p15.1337.ma>
2022-08-03 19:24:50 +0100

    building containers..

--- inception/mariadb/tools/launch_db.sh
+++ inception/mariadb/tools/launch_db.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
-# if [ ! -f "/var/lib/mysql/ib_buffer_pool" ]; then
 if [ ! -d /var/lib/mysql/$DB_NAME ]; then
-service mysql start
 
-# The wildcard as host implies that the user can connect from any client host
-echo "-----------\033[0;32mMARIA CREATE USER------------------\033[0m"
-
-echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
-echo "FLUSH PRIVILEGES" | mysql -u root
-
-echo "-----------\033[0;32mMARIA CREATE DATABASE------------------\033[0m"
-
-echo "CREATE DATABASE $DB_NAME;" | mysql -u root
-echo "FLUSH PRIVILEGES" | mysql -u root
-
-echo "-----------\033[0;32mMARIA UPDATE ROOT PASSWORD USER------------------\033[0m"
-
-echo "ALTER USER 'root'@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-echo "\033[0;32m--------------------------------\033[0m"
+    service mysql start
+    
+    echo "-----------\033[0;32mMySQL CREATE USER------------------\033[0m"
+    
+    echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+    echo "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+    echo "FLUSH PRIVILEGES" | mysql -u root
+    
+    echo "-----------\033[0;32mMySQL CREATE DATABASE------------------\033[0m"
+    
+    echo "CREATE DATABASE $DB_NAME;" | mysql -u root
+    echo "FLUSH PRIVILEGES" | mysql -u root
+    
+    echo "-----------\033[0;32mMySQL UPDATE ROOT PASSWORD USER------------------\033[0m"
+    
+    echo "ALTER USER 'root'@localhost IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
+    echo "FLUSH PRIVILEGES;" | mysql -u root
+    
+    echo "\033[0;32m--------------------------------\033[0m"
 
 fi 
 
 kill `cat /var/run/mysqld/mysqld.pid`
 
 mysqld_safe
-
-# tail -f /dev/null
